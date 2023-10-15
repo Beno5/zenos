0f132f77c88657591587d2b3e58f0c4718d55337
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2023-10-15 23:09:39 +0200

    devise tinymce blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth',
+                     path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'blog_posts/new'
   get 'blog_posts/edit'
-  get 'blog_posts/:id', to: 'blog_posts#show', as: 'blog_posts_show'
-  get 'pages/home'
+  resources :blog_posts
   get 'pages/about'
   get 'pages/projects'
   get 'pages/blog'
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
 end
