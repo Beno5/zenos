61b4898c7d1ec40269b893e0eef144d6417b2bc9
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2023-10-24 23:43:34 +0200

    kind of working

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link 'modernizr.min.js' 
+//= link 'plugins.min.js'
+//= link_directory ../../../vendor/css .css 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w[application.*.js]
 Rails.application.config.assets.precompile += %w[application.*.css]
 
 
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
